d41f6e4bea1dda399cd2579de892ed3e14439d02
Dellano Blake <dellano.blake@outlook.com>
2022-11-20 12:18:29 -0500

    debugged deployment errors

--- components/Header.js
+++ components/Header.js
@@ -17,6 +17,7 @@ function Header() {
                     width={40}
                     height={40}
                     layout="fixed"
+                    alt=''
                 />
                 <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                     <SearchIcon className='h-6 text-gray-600' />
@@ -48,6 +49,7 @@ function Header() {
                     width="40"
                     height="40"
                     layout="fixed"
+                    alt=''
                 />
                 <p className='whitespace-nowrap font-semibold pr-3'>{session.user.name}</p>
                 <ViewGridIcon className='icon' />
